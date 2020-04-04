3ac064834d439087c4ea58bf047520d761c09199
loich91 <51705844+loich91@users.noreply.github.com>
2020-04-04 15:07:20 +0200

    sq

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -2,15 +2,16 @@
 
 question=document.getElementById("question").innerHTML = "hello";
 reponse=document.getElementById("reponse").innerHTML="comment ça va?";
+reponseJuste=document.getElementById("reponseJuste");
 reponse=document.getElementById("reponse");
 point=0
 
 
 reponse.addEventListener("click", function (e) {
-    alert('je vais bien')
+
     point=point+1
     reponseCorrect=document.getElementById("point").innerHTML=point;
-
+    reponseJuste=document.getElementById("reponseJuste").innerHTML="je vais bien";
 
     e.preventDefault();
 });
