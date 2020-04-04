dd3aad7eb3f7a3ac194b3e988c88159d356c7485
loich91 <51705844+loich91@users.noreply.github.com>
2020-04-04 14:31:26 +0200

    premiere essai javascript

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,1 +1,22 @@
-alert("hello")
+
+
+question=document.getElementById("question").innerHTML = "hello";
+reponse=document.getElementById("reponse").innerHTML="comment ça va?";
+reponse=document.getElementById("reponse");
+point=0
+
+
+reponse.addEventListener("click", function (e) {
+    alert('je vais bien')
+    point=point+1
+    reponseCorrect=document.getElementById("point").innerHTML=point;
+
+
+    e.preventDefault();
+});
+
+
+
+
+
+
