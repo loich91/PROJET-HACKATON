40637386a8c7f1807b19755327104c02cc010f4b
yannickbrus <51903800+yannickbrus@users.noreply.github.com>
2020-04-04 23:51:05 +0200

    update index.php
    Nouvelles données

--- back-end/index.php
+++ back-end/index.php
@@ -7,10 +7,37 @@
 </head>
 <body>
 <?php
-// An indexed array
-$colors = array("Red", "Green", "Blue", "Orange");
- 
-echo json_encode($colors);
+
+
+$answer1;
+$answer2;
+$response;
+
+array(
+    "What does HTML stand for?" => array(
+        '1' => "Home Tool Markup Language",
+        '2' => "Hyperlinks and Text Markup Language",
+        '3' => "Hyper Text Markup Language",
+        '4' => "Hyper Text Manipulation Language",
+    ),
+    "Choose the correct HTML tag for the smallest heading:" => array(
+        '2' => "&lt;heading&gt;",
+        '1' => "&lt;h1&gt;",
+        '4' => "&lt;head&gt;",
+        '3' => "&lt;h6&gt;",
+    ),
+    );
+    
+    echo "<form>";
+    foreach($answer1 as $answer1 => $response) {
+        echo $response;
+        foreach($answer2 as $answer2 => $response) {
+            echo "<p> $response </p>";
+        }
+    }
+    echo "</form>";
 ?> 
 </body>
 </html>
+
+
